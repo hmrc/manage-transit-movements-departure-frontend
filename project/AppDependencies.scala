fe6819cada21f17e1f76d8408133387f4bf75ef1
Kevin <29205960+kevincoatsworth@users.noreply.github.com>
2022-05-06 14:38:18 +0100

    CTCP-380: Upgraded play and bootstrap dependencies.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-28"              % "0.60.0",
     "uk.gov.hmrc"                %% "logback-json-logger"             % "5.1.0",
     "uk.gov.hmrc"                %% "play-conditional-form-mapping"   % "1.10.0-play-28",
-    "uk.gov.hmrc"                %% "bootstrap-frontend-play-28"      % "5.19.0",
+    "uk.gov.hmrc"                %% "bootstrap-frontend-play-28"      % "5.24.0",
     "uk.gov.hmrc"                %% "play-allowlist-filter"           % "1.0.0-play-28",
     "uk.gov.hmrc"                %% "play-nunjucks"                   % "0.35.0-play-28",
     "uk.gov.hmrc"                %% "play-nunjucks-viewmodel"         % "0.15.0-play-28",
