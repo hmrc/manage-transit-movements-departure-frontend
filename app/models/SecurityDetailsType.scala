e6303b45ad402d68413f8383cff46d85b58d620a
carrielies <carrielies@users.noreply.github.com>
2022-05-13 13:13:48 +0100

    CTCP-406 Add SecurityDetailsType model

---
+++ app/models/SecurityDetailsType.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+sealed trait SecurityDetailsType
+
+object SecurityDetailsType extends RadioModel[SecurityDetailsType] {
+
+  case object NoSecurityDetails extends WithName("noSecurity") with SecurityDetailsType
+  case object EntrySummaryDeclarationSecurityDetails extends WithName("entrySummaryDeclaration") with SecurityDetailsType
+  case object EntryAndExitSummaryDeclarationSecurityDetails extends WithName("entryAndExitSummaryDeclaration") with SecurityDetailsType
+
+  override val messageKeyPrefix: String = "securityType"
+
+  override val values: Seq[SecurityDetailsType] = Seq(
+    NoSecurityDetails,
+    EntrySummaryDeclarationSecurityDetails,
+    EntryAndExitSummaryDeclarationSecurityDetails
+  )
+}
+
