dadfd6bcfb16db987daad9820e140a1739c4549c
carrielies <carrielies@users.noreply.github.com>
2022-05-18 14:27:16 +0100

    CTCP-365 ReAdd ignored migrations folders

---
+++ .g8/yesNoPage/migrations/$className__snake$.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo ""
+echo "Applying migration $className;format="snake"$"
+
+echo "Adding routes to conf/app.routes"
+
+echo "" >> ../conf/app.routes
+echo "GET        /:lrn/$className;format="decap"$                        controllers.$className$Controller.onPageLoad(lrn: LocalReferenceNumber, mode: Mode = NormalMode)" >> ../conf/app.routes
+echo "POST       /:lrn/$className;format="decap"$                        controllers.$className$Controller.onSubmit(lrn: LocalReferenceNumber, mode: Mode = NormalMode)" >> ../conf/app.routes
+
+echo "GET        /:lrn/change$className$                  controllers.$className$Controller.onPageLoad(lrn: LocalReferenceNumber, mode: Mode = CheckMode)" >> ../conf/app.routes
+echo "POST       /:lrn/change$className$                  controllers.$className$Controller.onSubmit(lrn: LocalReferenceNumber, mode: Mode = CheckMode)" >> ../conf/app.routes
+
+echo "Adding messages to conf.messages"
+echo "" >> ../conf/messages.en
+echo "$className;format="decap"$.title = $className;format="decap"$" >> ../conf/messages.en
+echo "$className;format="decap"$.heading = $className;format="decap"$" >> ../conf/messages.en
+echo "$className;format="decap"$.checkYourAnswersLabel = $className;format="decap"$" >> ../conf/messages.en
+echo "$className;format="decap"$.error.required = Select yes if $className;format="decap"$" >> ../conf/messages.en
+
+echo "Adding to UserAnswersEntryGenerators"
+awk '/self: Generators =>/ {\
+    print;\
+    print "";\
+    print "  implicit lazy val arbitrary$className$UserAnswersEntry: Arbitrary[($className$Page.type, JsValue)] =";\
+    print "    Arbitrary {";\
+    print "      for {";\
+    print "        value <- arbitrary[$className$Page.type#Data].map(Json.toJson(_))";\
+    print "      } yield ($className$Page, value)";\
+    print "    }";\
+    next }1' ../test/generators/UserAnswersEntryGenerators.scala > tmp && mv tmp ../test/generators/UserAnswersEntryGenerators.scala
+
+echo "Adding to UserAnswersGenerator"
+awk '/val generators/ {\
+    print;\
+    print "    arbitrary$className$UserAnswersEntry.arbitrary ::";\
+    next }1' ../test/generators/UserAnswersGenerator.scala > tmp && mv tmp ../test/generators/UserAnswersGenerator.scala
+
+echo "Migration $className;format="snake"$ completed"
