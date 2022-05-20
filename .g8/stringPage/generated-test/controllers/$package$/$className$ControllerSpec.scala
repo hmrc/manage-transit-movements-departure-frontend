57e91a96be576fcee35293ae9602a3120c651ec2
kenneth.rayner <31237148+kenneth-rayner@users.noreply.github.com>
2022-05-20 08:53:06 +0100

    CTCP-151: create scaffold for stringPage

---
+++ .g8/stringPage/generated-test/controllers/$package$/$className$ControllerSpec.scala
@@ -0,0 +1,145 @@
+package controllers.$package$
+
+import models.{NormalMode, UserAnswers}
+import navigation.Navigator
+import navigation.annotations.$navRoute$
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{reset, when}
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import forms.$package$.$className$FormProvider
+import views.html.$package$.$className$View
+import services.UserAnswersService
+import pages.$package$.$className$Page
+
+import scala.concurrent.Future
+
+class $className$ControllerSpec extends SpecBase with AppWithDefaultMockFixtures {
+
+  private val formProvider = new $className$FormProvider()
+  private val form         = formProvider()
+  private val mode         = NormalMode
+  private lazy val $className;format="decap"$Route = routes.$className$Controller.onPageLoad(lrn, mode).url
+  private lazy val mockUserAnswersService = mock[UserAnswersService]
+
+  override def guiceApplicationBuilder(): GuiceApplicationBuilder =
+    super
+      .guiceApplicationBuilder()
+      .overrides(bind(classOf[Navigator]).qualifiedWith(classOf[$navRoute$]).toInstance(fakeNavigator))
+      .overrides(bind[UserAnswersService].toInstance(mockUserAnswersService))
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    reset(mockUserAnswersService)
+  }
+
+  "$className$ Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(GET, $className;format="decap"$Route)
+
+      val result = route(app, request).value
+
+      val view = injector.instanceOf[$className$View]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(form, lrn, mode)(request, messages).toString
+
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = UserAnswers(lrn, eoriNumber).set($className$Page, true).success.value
+      setExistingUserAnswers(userAnswers)
+
+      val request = FakeRequest(GET, $className;format="decap"$Route)
+
+      val result = route(app, request).value
+
+      val filledForm = form.bind(Map("value" -> "true"))
+
+      val view = injector.instanceOf[$className$View]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(filledForm, lrn, mode)(request, messages).toString
+
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      setNoExistingUserAnswers()
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+      when(mockUserAnswersService.getOrCreateUserAnswers(any(), any())) thenReturn Future.successful(emptyUserAnswers)
+
+      val request =
+        FakeRequest(POST, $className;format="decap"$Route)
+          .withFormUrlEncodedBody(("value", "test string"))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual onwardRoute.url
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val invalidAnswer = ""
+
+      val request = FakeRequest(POST, $className;format="decap"$Route).withFormUrlEncodedBody(("value", ""))
+      val filledForm = form.bind(Map("value" -> invalidAnswer))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual BAD_REQUEST
+
+      val view = injector.instanceOf[$className$View]
+
+      contentAsString(result) mustEqual
+        view(filledForm, lrn, mode)(request, messages).toString
+
+    }
+
+    "must redirect to Session Expired for a GET if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(GET, $className;format="decap"$Route)
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+
+    }
+
+    "must redirect to Session Expired for a POST if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request =
+        FakeRequest(POST, $className;format="decap"$Route)
+          .withFormUrlEncodedBody(("value", "test string"))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+
+    }
+  }
+}
