a4912e08136168116e050c2bb224bdf01ae8527d
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-05-20 11:32:34 +0100

    CTCP-440: Scaffolded Transit Holder EORI Yes No page.

---
+++ app/navigation/TraderDetailsNavigator.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package navigation
+
+import models._
+import pages._
+import pages.traderDetails._
+import play.api.mvc.Call
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class TraderDetailsNavigator @Inject() () extends Navigator {
+
+  override val normalRoutes: PartialFunction[Page, UserAnswers => Option[Call]] = routes(NormalMode)
+
+  override val checkRoutes: PartialFunction[Page, UserAnswers => Option[Call]] = routes(CheckMode)
+
+  private def routes(mode: Mode): PartialFunction[Page, UserAnswers => Option[Call]] = {
+    case TransitHolderEoriYesNoPage => ua => ???
+  }
+
+}
