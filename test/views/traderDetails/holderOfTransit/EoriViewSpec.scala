533ddedd5d22243294fc219c06ecffc096df2b6b
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-05-23 11:28:10 +0100

    CTCP-441: Added hint to eori view.

--- test/views/traderDetails/holderOfTransit/EoriViewSpec.scala
+++ test/views/traderDetails/holderOfTransit/EoriViewSpec.scala
@@ -44,6 +44,8 @@ class EoriViewSpec extends InputTextViewBehaviours[String] {
 
   behave like pageWithHeading()
 
+  behave like pageWithHint("This will start 'GB' or 'XI' followed by 12 or 15 numbers, for example GB123456789000.")
+
   behave like pageWithInputText(Some(InputSize.Width20))
 
   behave like pageWithSubmitButton("Continue")
