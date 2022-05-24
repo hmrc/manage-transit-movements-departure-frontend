88a68ea28f0e7c0aa3617adc24027cf6e75913c8
carrielies <carrielies@users.noreply.github.com>
2022-05-24 11:12:19 +0100

    CTCP-151 Fix contentPage

--- .g8/contentPage/migrations/$className__snake$.sh
+++ .g8/contentPage/migrations/$className__snake$.sh
@@ -6,7 +6,7 @@ echo "Applying migration $className;format="snake"$"
 echo "Adding routes to conf/app.routes"
 
 echo "" >> ../conf/app.routes
-echo "GET        /:lrn/$package;format="space,normalize"$/$title;format="normalize"$                        controllers.$package$.$className$Controller.onPageLoad(lrn: LocalReferenceNumber, mode: Mode = NormalMode)" >> ../conf/app.routes
+echo "GET        /:lrn/$package;format="space,normalize"$/$title;format="normalize"$                        controllers.$package$.$className$Controller.onPageLoad(lrn: LocalReferenceNumber)" >> ../conf/app.routes
 
 echo "Adding messages to conf.messages"
 echo "" >> ../conf/messages.en
