53d6019edd891a6ea46cc1cbbac145c98af04417
carrielies <carrielies@users.noreply.github.com>
2022-06-07 12:59:02 +0100

    CTCP-452 Add new Route file to prod.routes

--- .g8/stringPage/migrations/$className__snake$.sh
+++ .g8/stringPage/migrations/$className__snake$.sh
@@ -6,14 +6,13 @@ echo "Applying migration $className;format="snake"$"
 echo "Adding routes to conf/app.$package$.routes"
 
 
-if [ ! -f ../conf/app.$package$.routes ]; then
-  echo "Write into file"
-  awk '
-  /"health.Routes"/ {
-   print "->         /manage-transit-movements/departure                   app.$package$.Routes"
-  }
-  { print }
-  ' ../conf/prod.routes
+if [ ! -f ./conf/app.$package$.routes ]; then
+  echo "Write into prod.routes file"
+  awk '/health.Routes/ {\
+    print;\
+    print "";\
+    print "->         /manage-transit-movements/departure                   app.$package$.Routes"
+    next }1' ../conf/prod.routes >> tmp && mv tmp ../conf/prod.routes
 fi
 
 echo "" >> ../conf/app.$package$.routes
