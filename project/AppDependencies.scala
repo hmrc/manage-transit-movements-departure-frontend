fc372ffb718971dd16eb610597e9c0dcb41c4207
kenneth.rayner <31237148+kenneth-rayner@users.noreply.github.com>
2022-06-13 09:50:02 +0100

    CTCP-dependencies: update to latest versions of dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,7 +22,7 @@ object AppDependencies {
     "org.scalatestplus.play"     %% "scalatestplus-play"       % "5.1.0",
     "org.scalatestplus"          %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "org.pegdown"                 % "pegdown"                  % "1.6.0",
-    "org.jsoup"                   % "jsoup"                    % "1.14.3",
+    "org.jsoup"                   % "jsoup"                    % "1.15.1",
     "com.typesafe.play"          %% "play-test"                % current,
     "org.mockito"                 % "mockito-core"             % "4.5.1",
     "org.scalacheck"             %% "scalacheck"               % "1.16.0",
