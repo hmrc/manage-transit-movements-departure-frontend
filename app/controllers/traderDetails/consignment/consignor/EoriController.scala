780a16bdc432168f1e13ad19fe19625e93e462cb
carrielies <carrielies@users.noreply.github.com>
2022-06-16 11:09:27 +0100

    CTCP-568 Add Eori No and YesNo Views

---
+++ app/controllers/traderDetails/consignment/consignor/EoriController.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.traderDetails.consignment.consignor
+
+import controllers.actions._
+import forms.EoriNumberFormProvider
+import models.{LocalReferenceNumber, Mode}
+import navigation.Navigator
+import navigation.annotations.HolderOfTransit
+import pages.traderDetails.consignment.consignor.EoriPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.traderDetails.consignment.consignor.EoriView
+import javax.inject.Inject
+import pages.traderDetails.consignment.consignor.EoriPage
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class EoriController @Inject() (
+  override val messagesApi: MessagesApi,
+  sessionRepository: SessionRepository,
+  @HolderOfTransit navigator: Navigator,
+  actions: Actions,
+  formProvider: EoriNumberFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  view: EoriView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  private val form = formProvider("traderDetails.consignment.consignor.eori")
+
+  def onPageLoad(lrn: LocalReferenceNumber, mode: Mode): Action[AnyContent] = actions.requireData(lrn) {
+    implicit request =>
+      val preparedForm = request.userAnswers.get(EoriPage) match {
+        case None        => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, lrn, mode))
+  }
+
+  def onSubmit(lrn: LocalReferenceNumber, mode: Mode): Action[AnyContent] = actions.requireData(lrn).async {
+    implicit request =>
+      form
+        .bindFromRequest()
+        .fold(
+          formWithErrors => Future.successful(BadRequest(view(formWithErrors, lrn, mode))),
+          value =>
+            for {
+              updatedAnswers <- Future.fromTry(request.userAnswers.set(EoriPage, value))
+              _              <- sessionRepository.set(updatedAnswers)
+            } yield Redirect(navigator.nextPage(EoriPage, mode, updatedAnswers))
+        )
+  }
+}
