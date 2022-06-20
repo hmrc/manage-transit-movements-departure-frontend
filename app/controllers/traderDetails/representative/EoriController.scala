b923fb3c3de1fe10cf958b3c0d6b53c3b16ab83b
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2022-06-20 11:51:09 +0100

    Added ops to Representative

--- app/controllers/traderDetails/representative/EoriController.scala
+++ app/controllers/traderDetails/representative/EoriController.scala
@@ -27,14 +27,15 @@ import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.traderDetails.representative.EoriView
+import controllers.{SettableOps, SettableOpsRunner}
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 class EoriController @Inject() (
   override val messagesApi: MessagesApi,
-  sessionRepository: SessionRepository,
-  @Representative navigator: Navigator,
+  implicit val sessionRepository: SessionRepository,
+  @Representative implicit val navigator: Navigator,
   formProvider: EoriNumberFormProvider,
   actions: Actions,
   val controllerComponents: MessagesControllerComponents,
@@ -60,11 +61,7 @@ class EoriController @Inject() (
         .bindFromRequest()
         .fold(
           formWithErrors => Future.successful(BadRequest(view(formWithErrors, lrn, mode))),
-          value =>
-            for {
-              updatedAnswers <- Future.fromTry(request.userAnswers.set(EoriPage, value))
-              _              <- sessionRepository.set(updatedAnswers)
-            } yield Redirect(navigator.nextPage(EoriPage, mode, updatedAnswers))
+          value => EoriPage.userAnswerWriter(value).writeToSessionNavigator(mode)
         )
   }
 }
