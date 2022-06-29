8787d7240f44f9304856941b10c7b8368dee6e22
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-06-29 15:17:20 +0100

    Removed user answer entry generators from g8 migration scripts. These will instead be added manually.

--- .g8/datePage/migrations/$className__snake$.sh
+++ .g8/datePage/migrations/$className__snake$.sh
@@ -23,23 +23,4 @@ echo "$package$.$className;format="decap"$.error.required.two = The date for $ti
 echo "$package$.$className;format="decap"$.error.required = The date for $title$ must include {0}" >> ../conf/messages.en
 echo "$package$.$className;format="decap"$.error.invalid = Enter a real date for $title$" >> ../conf/messages.en
 
-
-echo "Adding to UserAnswersEntryGenerators"
-awk '/self: Generators =>/ {\
-    print;\
-    print "";\
-    print "  implicit lazy val arbitrary$package;format="space,upper-camel"$$className$UserAnswersEntry: Arbitrary[(pages.$package$.$className$Page.type, JsValue)] =";\
-    print "    Arbitrary {";\
-    print "      for {";\
-    print "        value <- arbitrary[pages.$package$.$className$Page.type#Data].map(Json.toJson(_))";\
-    print "      } yield (pages.$package$.$className$Page, value)";\
-    print "    }";\
-    next }1' ../test/generators/UserAnswersEntryGenerators.scala > tmp && mv tmp ../test/generators/UserAnswersEntryGenerators.scala
-
-echo "Adding to UserAnswersGenerator"
-awk '/val generators/ {\
-    print;\
-    print "    arbitrary$package;format="space,upper-camel"$$className$UserAnswersEntry.arbitrary ::";\
-    next }1' ../test/generators/UserAnswersGenerator.scala > tmp && mv tmp ../test/generators/UserAnswersGenerator.scala
-
 echo "Migration $className;format="snake"$ completed"
--- .g8/radioButtonPage/migrations/$className__snake$.sh
+++ .g8/radioButtonPage/migrations/$className__snake$.sh
@@ -21,32 +21,14 @@ echo "$package$.$className;format="decap"$.$option2key;format="decap"$ = $option
 echo "$package$.$className;format="decap"$.checkYourAnswersLabel = $title$" >> ../conf/messages.en
 echo "$package$.$className;format="decap"$.error.required = Select  $title$" >> ../conf/messages.en
 
-echo "Adding to UserAnswersEntryGenerators"
-awk '/self: Generators =>/ {\
-    print;\
-    print "";\
-    print "  implicit lazy val arbitrary$package;format="space,upper-camel"$$className$UserAnswersEntry: Arbitrary[(pages.$package$.$className$Page.type, JsValue)] =";\
-    print "    Arbitrary {";\
-    print "      for {";\
-    print "        value <- arbitrary[pages.$package$.$className$Page.type#Data].map(Json.toJson(_))";\
-    print "      } yield (pages.$package$.$className$Page, value)";\
-    print "    }";\
-    next }1' ../test/generators/UserAnswersEntryGenerators.scala > tmp && mv tmp ../test/generators/UserAnswersEntryGenerators.scala
-
 echo "Adding to ModelGenerators"
 awk '/self: Generators =>/ {\
     print;\
     print "";\
     print "  implicit lazy val arbitrary$className$: Arbitrary[models.$package$.$className$] =";\
     print "    Arbitrary {";\
-    print "      Gen.oneOf(models.$package$.$className$.values.toSeq)";\
+    print "      Gen.oneOf(models.$package$.$className$.values)";\
     print "    }";\
     next }1' ../test/generators/ModelGenerators.scala > tmp && mv tmp ../test/generators/ModelGenerators.scala
-
-echo "Adding to UserAnswersGenerator"
-awk '/val generators/ {\
-    print;\
-    print "    arbitrary$package;format="space,upper-camel"$$className$UserAnswersEntry.arbitrary ::";\
-    next }1' ../test/generators/UserAnswersGenerator.scala > tmp && mv tmp ../test/generators/UserAnswersGenerator.scala
 
 echo "Migration $className;format="snake"$ completed"
