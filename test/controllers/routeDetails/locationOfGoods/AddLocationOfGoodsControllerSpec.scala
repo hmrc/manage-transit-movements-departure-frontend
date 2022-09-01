12a550e967b672448fb1ed6d100b495e5799ae84
kenneth.rayner <31237148+kenneth-rayner@users.noreply.github.com>
2022-09-01 11:28:31 +0100

    CTCP-781: create add location of goods page

---
+++ test/controllers/routeDetails/locationOfGoods/AddLocationOfGoodsControllerSpec.scala
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.routeDetails.locationOfGoods
+
+import base.{AppWithDefaultMockFixtures, SpecBase}
+import forms.YesNoFormProvider
+import models.NormalMode
+import navigation.Navigator
+import navigation.annotations.PreTaskListDetails
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.routeDetails.locationOfGoods.AddLocationOfGoodsPage
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import views.html.routeDetails.locationOfGoods.AddLocationOfGoodsView
+
+import scala.concurrent.Future
+
+class AddLocationOfGoodsControllerSpec extends SpecBase with AppWithDefaultMockFixtures with MockitoSugar {
+
+  private val formProvider                 = new YesNoFormProvider()
+  private val form                         = formProvider("routeDetails.locationOfGoods.addLocationOfGoods")
+  private val mode                         = NormalMode
+  private lazy val addLocationOfGoodsRoute = routes.AddLocationOfGoodsController.onPageLoad(lrn, mode).url
+
+  override def guiceApplicationBuilder(): GuiceApplicationBuilder =
+    super
+      .guiceApplicationBuilder()
+      .overrides(bind(classOf[Navigator]).qualifiedWith(classOf[PreTaskListDetails]).toInstance(fakeNavigator))
+
+  "AddLocationOfGoods Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val request = FakeRequest(GET, addLocationOfGoodsRoute)
+      val result  = route(app, request).value
+
+      val view = injector.instanceOf[AddLocationOfGoodsView]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(form, lrn, mode)(request, messages).toString
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = emptyUserAnswers.setValue(AddLocationOfGoodsPage, true)
+      setExistingUserAnswers(userAnswers)
+
+      val request = FakeRequest(GET, addLocationOfGoodsRoute)
+
+      val result = route(app, request).value
+
+      val filledForm = form.bind(Map("value" -> "true"))
+
+      val view = injector.instanceOf[AddLocationOfGoodsView]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(filledForm, lrn, mode)(request, messages).toString
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val request = FakeRequest(POST, addLocationOfGoodsRoute)
+        .withFormUrlEncodedBody(("value", "true"))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual onwardRoute.url
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val request   = FakeRequest(POST, addLocationOfGoodsRoute).withFormUrlEncodedBody(("value", ""))
+      val boundForm = form.bind(Map("value" -> ""))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual BAD_REQUEST
+
+      val view = injector.instanceOf[AddLocationOfGoodsView]
+
+      contentAsString(result) mustEqual
+        view(boundForm, lrn, mode)(request, messages).toString
+    }
+
+    "must redirect to Session Expired for a GET if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(GET, addLocationOfGoodsRoute)
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+    }
+
+    "must redirect to Session Expired for a POST if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(POST, addLocationOfGoodsRoute)
+        .withFormUrlEncodedBody(("value", "true"))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+    }
+  }
+}
