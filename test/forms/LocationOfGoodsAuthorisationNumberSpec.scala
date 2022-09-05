87dad6fe3d642fc7db0035b72c86da2f81930276
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2022-09-05 15:50:17 +0100

    CTCP-804: Add LocationOfGoodsAuthorisationNumber page.

---
+++ test/forms/LocationOfGoodsAuthorisationNumberSpec.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.Constants.maxLocationOfGoodsAuthorisationNumberLength
+import forms.behaviours.StringFieldBehaviours
+import org.scalacheck.Gen
+import play.api.data.{Field, FormError}
+import models.domain.StringFieldRegex.alphaNumericRegex
+
+class LocationOfGoodsAuthorisationNumberSpec extends StringFieldBehaviours {
+
+  private val prefix               = Gen.alphaNumStr.sample.value
+  private val invalidCharactersKey = s"$prefix.error.invalidCharacters"
+  private val requiredKey          = s"$prefix.error.required"
+  private val maxLengthKey         = s"$prefix.error.maxLength"
+  private val maxLength            = 35
+
+  val form = new LocationOfGoodsAuthorisationNumberFormProvider()(prefix)
+
+  ".value" - {
+
+    val fieldName = "value"
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      stringsWithMaxLength(maxLength)
+    )
+
+    behave like fieldWithInvalidCharacters(
+      form,
+      fieldName,
+      error = FormError(fieldName, invalidCharactersKey, Seq(alphaNumericRegex.regex)),
+      maxLocationOfGoodsAuthorisationNumberLength
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+
+    "must not bind valid strings over max length" in {
+      val expectedError = FormError(fieldName, maxLengthKey, Seq(maxLocationOfGoodsAuthorisationNumberLength))
+
+      val gen = for {
+        str <- stringsLongerThan(maxLocationOfGoodsAuthorisationNumberLength, Gen.alphaNumChar)
+      } yield str
+
+      forAll(gen) {
+        invalidString =>
+          val result: Field = form.bind(Map(fieldName -> invalidString)).apply(fieldName)
+          result.errors must contain(expectedError)
+      }
+    }
+  }
+}
