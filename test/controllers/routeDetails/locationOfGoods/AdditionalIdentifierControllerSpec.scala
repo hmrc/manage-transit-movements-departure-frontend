eb2f27968d30f0c571d15c63df6f43a3fc9ba7a6
kenneth.rayner <31237148+kenneth-rayner@users.noreply.github.com>
2022-09-09 08:25:20 +0100

    CTCP-813: create Additional Identifier page

---
+++ test/controllers/routeDetails/locationOfGoods/AdditionalIdentifierControllerSpec.scala
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.routeDetails.locationOfGoods
+
+import base.{AppWithDefaultMockFixtures, SpecBase}
+import forms.AdditionalIdentifierFormProvider
+import models.{NormalMode, UserAnswers}
+import navigation.Navigator
+import navigation.annotations.PreTaskListDetails
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{reset, when}
+import pages.routeDetails.locationOfGoods.AdditionalIdentifierPage
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import views.html.routeDetails.locationOfGoods.AdditionalIdentifierView
+
+import scala.concurrent.Future
+
+class AdditionalIdentifierControllerSpec extends SpecBase with AppWithDefaultMockFixtures {
+
+  private val formProvider                   = new AdditionalIdentifierFormProvider()
+  private val form                           = formProvider("routeDetails.locationOfGoods.additionalIdentifier")
+  private val mode                           = NormalMode
+  private lazy val additionalIdentifierRoute = routes.AdditionalIdentifierController.onPageLoad(lrn, mode).url
+
+  override def guiceApplicationBuilder(): GuiceApplicationBuilder =
+    super
+      .guiceApplicationBuilder()
+      .overrides(bind(classOf[Navigator]).qualifiedWith(classOf[PreTaskListDetails]).toInstance(fakeNavigator))
+
+  "AdditionalIdentifier Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val request = FakeRequest(GET, additionalIdentifierRoute)
+
+      val result = route(app, request).value
+
+      val view = injector.instanceOf[AdditionalIdentifierView]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(form, lrn, mode)(request, messages).toString
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = emptyUserAnswers.setValue(AdditionalIdentifierPage, "test")
+      setExistingUserAnswers(userAnswers)
+
+      val request = FakeRequest(GET, additionalIdentifierRoute)
+
+      val result = route(app, request).value
+
+      val filledForm = form.bind(Map("value" -> "test"))
+
+      val view = injector.instanceOf[AdditionalIdentifierView]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(filledForm, lrn, mode)(request, messages).toString
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      when(mockSessionRepository.set(any())(any())) thenReturn Future.successful(true)
+
+      val request = FakeRequest(POST, additionalIdentifierRoute)
+        .withFormUrlEncodedBody(("value", "test"))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual onwardRoute.url
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val invalidAnswer = ""
+
+      val request    = FakeRequest(POST, additionalIdentifierRoute).withFormUrlEncodedBody(("value", ""))
+      val filledForm = form.bind(Map("value" -> invalidAnswer))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual BAD_REQUEST
+
+      val view = injector.instanceOf[AdditionalIdentifierView]
+
+      contentAsString(result) mustEqual
+        view(filledForm, lrn, mode)(request, messages).toString
+    }
+
+    "must redirect to Session Expired for a GET if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(GET, additionalIdentifierRoute)
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+    }
+
+    "must redirect to Session Expired for a POST if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(POST, additionalIdentifierRoute)
+        .withFormUrlEncodedBody(("value", "test string"))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+    }
+  }
+}
