27f1261d40ce2d4f415e641f45a89f0e67f8958c
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-09-20 14:01:37 +0100

    Amending unloading section to be of type JsObject

--- app/pages/sections/routeDetails/unloading/UnloadingSection.scala
+++ app/pages/sections/routeDetails/unloading/UnloadingSection.scala
@@ -18,9 +18,9 @@ package pages.sections.routeDetails.unloading
 
 import pages.QuestionPage
 import pages.sections.routeDetails.RouteDetailsSection
-import play.api.libs.json.JsPath
+import play.api.libs.json.{JsObject, JsPath}
 
-case object UnloadingSection extends QuestionPage[Nothing] {
+case object UnloadingSection extends QuestionPage[JsObject] {
 
   override def path: JsPath = RouteDetailsSection.path \ toString
 
