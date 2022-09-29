4380c19d3628eeb06938bb89ddaadc363e728098
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-09-29 16:17:38 +0100

    CTCP-1280: Added cleanup lofic for T2DeclarationTypeYesNoPage

--- test/pages/routeDetails/transit/T2DeclarationTypeYesNoPageSpec.scala
+++ test/pages/routeDetails/transit/T2DeclarationTypeYesNoPageSpec.scala
@@ -16,7 +16,11 @@
 
 package pages.routeDetails.transit
 
+import models.DateTime
+import models.reference.{Country, CustomsOffice}
+import org.scalacheck.Arbitrary.arbitrary
 import pages.behaviours.PageBehaviours
+import pages.routeDetails.transit.index._
 
 class T2DeclarationTypeYesNoPageSpec extends PageBehaviours {
 
@@ -27,5 +31,55 @@ class T2DeclarationTypeYesNoPageSpec extends PageBehaviours {
     beSettable[Boolean](T2DeclarationTypeYesNoPage)
 
     beRemovable[Boolean](T2DeclarationTypeYesNoPage)
+
+    "cleanup" - {
+      val transitCountry       = arbitrary[Country].sample.value
+      val transitCustomsOffice = arbitrary[CustomsOffice].sample.value
+      val eta                  = arbitrary[DateTime].sample.value
+
+      "when answer changes" - {
+        "must clean up add office of transit yes/no and offices of transit" in {
+          val bool = arbitrary[Boolean].sample.value
+
+          val preChange = emptyUserAnswers
+            .setValue(T2DeclarationTypeYesNoPage, bool)
+            .setValue(AddOfficeOfTransitYesNoPage, true)
+            .setValue(OfficeOfTransitCountryPage(index), transitCountry)
+            .setValue(OfficeOfTransitPage(index), transitCustomsOffice)
+            .setValue(AddOfficeOfTransitETAYesNoPage(index), true)
+            .setValue(OfficeOfTransitETAPage(index), eta)
+
+          val postChange = preChange.setValue(T2DeclarationTypeYesNoPage, !bool)
+
+          postChange.get(AddOfficeOfTransitYesNoPage) mustNot be(defined)
+          postChange.get(OfficeOfTransitCountryPage(index)) mustNot be(defined)
+          postChange.get(OfficeOfTransitPage(index)) mustNot be(defined)
+          postChange.get(AddOfficeOfTransitETAYesNoPage(index)) mustNot be(defined)
+          postChange.get(OfficeOfTransitETAPage(index)) mustNot be(defined)
+        }
+      }
+
+      "when answer doesn't change" - {
+        "must do nothing" in {
+          val bool = arbitrary[Boolean].sample.value
+
+          val preChange = emptyUserAnswers
+            .setValue(T2DeclarationTypeYesNoPage, bool)
+            .setValue(AddOfficeOfTransitYesNoPage, true)
+            .setValue(OfficeOfTransitCountryPage(index), transitCountry)
+            .setValue(OfficeOfTransitPage(index), transitCustomsOffice)
+            .setValue(AddOfficeOfTransitETAYesNoPage(index), true)
+            .setValue(OfficeOfTransitETAPage(index), eta)
+
+          val postChange = preChange.setValue(T2DeclarationTypeYesNoPage, bool)
+
+          postChange.get(AddOfficeOfTransitYesNoPage) must be(defined)
+          postChange.get(OfficeOfTransitCountryPage(index)) must be(defined)
+          postChange.get(OfficeOfTransitPage(index)) must be(defined)
+          postChange.get(AddOfficeOfTransitETAYesNoPage(index)) must be(defined)
+          postChange.get(OfficeOfTransitETAPage(index)) must be(defined)
+        }
+      }
+    }
   }
 }
