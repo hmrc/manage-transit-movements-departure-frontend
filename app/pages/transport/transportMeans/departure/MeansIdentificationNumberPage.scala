e4c6a5e22750bb542928edb1da4e12f68678888d
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-10-28 12:17:06 +0100

    CTCP-1532: Removed redundant section.

--- app/pages/transport/transportMeans/departure/MeansIdentificationNumberPage.scala
+++ app/pages/transport/transportMeans/departure/MeansIdentificationNumberPage.scala
@@ -19,13 +19,13 @@ package pages.transport.transportMeans.departure
 import controllers.transport.transportMeans.departure.routes
 import models.{Mode, UserAnswers}
 import pages.QuestionPage
-import pages.sections.TransportMeans
+import pages.sections.transport.TransportMeansSection
 import play.api.libs.json.JsPath
 import play.api.mvc.Call
 
 case object MeansIdentificationNumberPage extends QuestionPage[String] {
 
-  override def path: JsPath = TransportMeans.path \ toString
+  override def path: JsPath = TransportMeansSection.path \ toString
 
   override def toString: String = "meansIdentificationNumber"
 
