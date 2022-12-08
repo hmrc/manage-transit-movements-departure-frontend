e523b66a871313fbee13608681eb783e229f1906
kevin.coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2022-12-08 14:44:16 +0000

    CTCP-1883: WIP - started to add alternative sub domain readers

--- app/models/journeyDomain/transport/TransportMeansActiveDomain.scala
+++ app/models/journeyDomain/transport/TransportMeansActiveDomain.scala
@@ -20,6 +20,7 @@ import cats.implicits._
 import models.Index
 import models.SecurityDetailsType.NoSecurityDetails
 import models.domain.{GettableAsFilterForNextReaderOps, GettableAsReaderOps, UserAnswersReader}
+import models.journeyDomain.JourneyDomainModel
 import models.reference.{CustomsOffice, Nationality}
 import models.transport.transportMeans.BorderModeOfTransport
 import models.transport.transportMeans.active.Identification
@@ -28,7 +29,88 @@ import pages.preTaskList.SecurityDetailsTypePage
 import pages.transport.transportMeans.BorderModeOfTransportPage
 import pages.transport.transportMeans.active._
 
-case class TransportMeansActiveDomain(
+sealed trait TransportMeansActiveDomain extends JourneyDomainModel {
+  val borderMode: BorderModeOfTransport
+  val identification: Identification
+}
+
+object TransportMeansActiveDomain {
+
+  implicit val userAnswersReader: UserAnswersReader[TransportMeansActiveDomain] =
+    BorderModeOfTransportPage.reader.flatMap {
+      case BorderModeOfTransport.Rail => UserAnswersReader[TransportMeansDomainWithRailBorderMode].widen[TransportMeansActiveDomain]
+      case BorderModeOfTransport.Road => UserAnswersReader[TransportMeansDomainWithRoadBorderMode].widen[TransportMeansActiveDomain]
+      case _                          => UserAnswersReader[TransportMeansDomainWithAnyOtherBorderMode]
+    }
+}
+
+case class TransportMeansDomainWithRailBorderMode(
+  identificationNumber: String,
+  nationality: Nationality
+) extends TransportMeansActiveDomain {
+  override val borderMode: BorderModeOfTransport = BorderModeOfTransport.Rail
+  override val identification: Identification    = Identification.TrainNumber
+}
+
+object TransportMeansDomainWithRailBorderMode {
+
+  def conveyanceReads(index: Index): UserAnswersReader[Option[String]] = {
+
+    val details = for {
+      securityDetails <- SecurityDetailsTypePage.reader
+      borderMode      <- BorderModeOfTransportPage.reader
+    } yield (securityDetails, borderMode)
+
+    details.flatMap {
+      case (NoSecurityDetails, x: BorderModeOfTransport) if x != Air =>
+        ConveyanceReferenceNumberYesNoPage(index).filterOptionalDependent(identity)(ConveyanceReferenceNumberPage(index).reader)
+      case _ => ConveyanceReferenceNumberPage(index).reader.map(Some(_))
+    }
+  }
+
+  implicit def userAnswersReader(index: Index): UserAnswersReader[TransportMeansDomainWithRailBorderMode] =
+    (
+      IdentificationNumberPage(index).reader,
+      AddNationalityYesNoPage(index).filterOptionalDependent(identity)(NationalityPage(index).reader),
+      CustomsOfficeActiveBorderPage(index).reader,
+      conveyanceReads(index)
+    ).tupled.map((TransportMeansDomainWithRailBorderMode.apply _).tupled)
+}
+
+case class TransportMeansDomainWithRoadBorderMode(
+  identificationNumber: String,
+  nationality: Nationality
+) extends TransportMeansActiveDomain {
+  override val borderMode: BorderModeOfTransport = BorderModeOfTransport.Road
+  override val identification: Identification    = Identification.RegNumberRoadVehicle
+}
+
+object TransportMeansDomainWithRoadBorderMode {
+
+  def conveyanceReads(index: Index): UserAnswersReader[Option[String]] = {
+
+    val details = for {
+      securityDetails <- SecurityDetailsTypePage.reader
+      borderMode      <- BorderModeOfTransportPage.reader
+    } yield (securityDetails, borderMode)
+
+    details.flatMap {
+      case (NoSecurityDetails, x: BorderModeOfTransport) if x != Air =>
+        ConveyanceReferenceNumberYesNoPage(index).filterOptionalDependent(identity)(ConveyanceReferenceNumberPage(index).reader)
+      case _ => ConveyanceReferenceNumberPage(index).reader.map(Some(_))
+    }
+  }
+
+  implicit def userAnswersReader(index: Index): UserAnswersReader[TransportMeansDomainWithRoadBorderMode] =
+    (
+      IdentificationNumberPage(index).reader,
+      AddNationalityYesNoPage(index).filterOptionalDependent(identity)(NationalityPage(index).reader),
+      CustomsOfficeActiveBorderPage(index).reader,
+      conveyanceReads(index)
+    ).tupled.map((TransportMeansDomainWithRoadBorderMode.apply _).tupled)
+}
+
+case class TransportMeansDomainWithAnyOtherBorderMode(
   identification: Identification,
   identificationNumber: String,
   nationality: Option[Nationality],
@@ -36,7 +118,7 @@ case class TransportMeansActiveDomain(
   conveyanceReferenceNumber: Option[String]
 )
 
-object TransportMeansActiveDomain {
+object TransportMeansDomainWithAnyOtherBorderMode {
 
   def conveyanceReads(index: Index): UserAnswersReader[Option[String]] = {
 
@@ -64,6 +146,5 @@ object TransportMeansActiveDomain {
       AddNationalityYesNoPage(index).filterOptionalDependent(identity)(NationalityPage(index).reader),
       CustomsOfficeActiveBorderPage(index).reader,
       conveyanceReads(index)
-    ).tupled.map((TransportMeansActiveDomain.apply _).tupled)
-
+    ).tupled.map((TransportMeansDomainWithAnyOtherBorderMode.apply _).tupled)
 }
