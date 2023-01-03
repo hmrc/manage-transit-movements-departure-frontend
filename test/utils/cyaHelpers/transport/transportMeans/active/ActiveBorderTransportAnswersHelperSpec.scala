a4241b615de91f907a27db517c7bf12383685d93
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2023-01-03 14:17:37 +0000

    CTCP-1934: fix test

--- test/utils/cyaHelpers/transport/transportMeans/active/ActiveBorderTransportAnswersHelperSpec.scala
+++ test/utils/cyaHelpers/transport/transportMeans/active/ActiveBorderTransportAnswersHelperSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -228,7 +228,7 @@ class ActiveBorderTransportAnswersHelperSpec extends SpecBase with ScalaCheckPro
 
               result mustBe Some(
                 SummaryListRow(
-                  key = Key("Office of transit".toText),
+                  key = Key("Customs office".toText),
                   value = Value(s"$customsOffice".toText),
                   actions = Some(
                     Actions(
