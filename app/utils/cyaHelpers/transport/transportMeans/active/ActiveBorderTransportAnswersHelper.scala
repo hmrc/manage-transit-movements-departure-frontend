cfc296a6f9b469c0279df6877b45d7e8e979e1da
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2023-01-03 12:12:14 +0000

    CTCP-1934: update content

--- app/utils/cyaHelpers/transport/transportMeans/active/ActiveBorderTransportAnswersHelper.scala
+++ app/utils/cyaHelpers/transport/transportMeans/active/ActiveBorderTransportAnswersHelper.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,8 @@ import pages.transport.transportMeans.active._
 import play.api.i18n.Messages
 import uk.gov.hmrc.govukfrontend.views.Aliases.SummaryListRow
 import utils.cyaHelpers.AnswersHelper
+
+import scala.Console.println
 
 class ActiveBorderTransportAnswersHelper(
   userAnswers: UserAnswers,
