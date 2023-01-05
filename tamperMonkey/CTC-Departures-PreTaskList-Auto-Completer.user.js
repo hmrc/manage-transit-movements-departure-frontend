b50390cfea146188bd934d65b4f7ab27621984cc
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2023-01-05 12:33:53 +0000

    Cleaned up the scripts and seperated functionality into standalone methods

--- tamperMonkey/CTC-Departures-PreTaskList-Auto-Completer.user.js
+++ tamperMonkey/CTC-Departures-PreTaskList-Auto-Completer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CTC-Departures PreTaskList Auto Completer
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      3.0
 // @description  Script to automatically fill out CTC sections
 // @author       Reece-Carruthers
 // @match        http*://*/manage-transit-movements/what-do-you-want-to-do
@@ -19,26 +19,56 @@
 })();
 
 window.addEventListener('load', function() {
-    var preTaskT1NoSecurityButtonPressed = GM_getValue('preTaskT1NoSecurityButtonPressed',false)
 
-    // Generate random numbers for LRN if LRN page is hit
-    if(location.href.includes('manage-transit-movements/departures/local-reference-number')){
-        GM_setValue('lrn', Math.floor((Math.random() + 1) * 10000))
-    }
-    if(preTaskT1NoSecurityButtonPressed){
+    generateLRN()
+    isAButtonToggled()
+
+}, false);
+
+/* Main Functions */
+
+function isAButtonToggled() {
+    if(GM_getValue('preTaskT1NoSecurityToggle',false)){
         preTaskListT1NoSecurity()
     }else {
         if(!location.href.includes('task-list')) {
-            document.body.appendChild(setup())
+            document.body.appendChild(setupGUI())
         }
     }
-}, false);
+}
 
-function setup() {
-    var panel = document.createElement('div')
+function toggleButtonsOff() {
+    GM_setValue('preTaskT1NoSecurityToggle', false)
+}
+
+function setupGUI() {
+    const panel = document.createElement('div')
     panel.appendChild(createPreTaskT1NoSecurityButton())
     return panel
 }
+
+/* Helper Functions */
+
+function generateLRN() {
+    if(location.href.includes('manage-transit-movements/departures/local-reference-number')){
+        GM_setValue('lrn', Math.floor((Math.random() + 1) * 10000))
+    }
+}
+
+function getLRN() {
+    return GM_getValue('lrn', null)
+}
+
+const currentPageIs = (path) => {
+    if(path.includes("*")) {
+        let matches = window.location.pathname.match(path)
+        return matches && window.location.pathname.endsWith(path.slice(-5))
+    } else {
+        return path === window.location.pathname
+    }
+}
+
+/* Buttons */
 
 function createPreTaskT1NoSecurityButton() {
     let button = document.createElement('button')
@@ -54,20 +84,11 @@ function createPreTaskT1NoSecurityButton() {
             button.style.top = '50px'
             button.innerHTML = 'Complete Pretask List (T1/No Security)'
             button.addEventListener("click", function handleClick() {
-                GM_setValue('preTaskT1NoSecurityButtonPressed',true)
+                GM_setValue('preTaskT1NoSecurityToggle',true)
                 preTaskListT1NoSecurity()
             })
 
             return button
-}
-
-const currentPageIs = (path) => {
-    if(path.includes("*")) {
-        let matches = window.location.pathname.match(path)
-        return matches && window.location.pathname.endsWith(path.slice(-5))
-    } else {
-        return path === window.location.pathname
-    }
 }
 
 /* #### PreTaskList Pages #### */
@@ -120,26 +141,24 @@ const preTaskListCYA = (lrn) => {
     }
 }
 
-/* Sets button press to false on the landing page to prevent script running once you've went through the journey */
 const taskListPage = (lrn) => {
     if(currentPageIs(`/manage-transit-movements/departures/${lrn}/task-list`)){
-        GM_setValue('preTaskT1NoSecurityButtonPressed', false)
+        toggleButtonsOff()
     }
 }
 /* #### Journeys #### */
 
-/* ## Pre task list journey ## */
+/* ## Pre-task list journey ## */
 
 function preTaskListT1NoSecurity() {
     departureDeclarationPage()
-    var lrn = GM_getValue('lrn', null)
-    lrnPage(lrn)
-    officeOfDeparturePage(lrn, 'GB000068')
-    procedureTypePage(lrn, 'value')
-    declarationTypePage(lrn, 'value')
-    securityDetails(lrn, 'value')
-    preTaskListCYA(lrn)
-    taskListPage(lrn)
+    lrnPage(getLRN())
+    officeOfDeparturePage(getLRN(), 'GB000068')
+    procedureTypePage(getLRN(), 'value')
+    declarationTypePage(getLRN(), 'value')
+    securityDetails(getLRN(), 'value')
+    preTaskListCYA(getLRN())
+    taskListPage(getLRN())
 }
 
 
