0dbdb2f228b7e881bdbf7e8203aa3b7ddf1b2006
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2023-01-23 09:44:00 +0000

    PluginUpdate: Update plugins to latest allowable versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     "uk.gov.hmrc"                %% "play-conditional-form-mapping"   % "1.12.0-play-28",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-28"      % bootstrapVersion,
     "uk.gov.hmrc"                %% "play-allowlist-filter"           % "1.1.0",
-    "uk.gov.hmrc"                %% "play-frontend-hmrc"              % "6.0.0-play-28",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc"              % "6.2.0-play-28",
     "org.typelevel"              %% "cats-core"                       % "2.9.0",
     "com.chuusai"                %% "shapeless"                       % "2.3.10"
   )
