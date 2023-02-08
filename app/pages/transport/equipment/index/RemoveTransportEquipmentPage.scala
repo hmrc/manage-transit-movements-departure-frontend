5dd170cf54bb16e59b7c90235aeeef9093e1ae37
kevin.coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2023-02-08 16:24:06 +0000

    CTCP-1874: Initial scaffold

---
+++ app/pages/transport/equipment/index/RemoveTransportEquipmentPage.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pages.transport.equipment.index
+
+import controllers.transport.equipment.index.routes
+import models.{Mode, UserAnswers}
+import pages.QuestionPage
+import pages.sections.EquipmentsSection
+import play.api.libs.json.JsPath
+import play.api.mvc.Call
+
+case object RemoveTransportEquipmentPage extends QuestionPage[Boolean] {
+
+  override def path: JsPath = EquipmentsSection.path \ toString
+
+  override def toString: String = "removeTransportEquipment"
+
+  override def route(userAnswers: UserAnswers, mode: Mode): Option[Call] =
+    Some(routes.RemoveTransportEquipmentController.onPageLoad(userAnswers.lrn, mode))
+}
---
+++ test/views/transport/equipment/index/RemoveTransportEquipmentViewSpec.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.transport.equipment.index
+
+import models.NormalMode
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import views.behaviours.YesNoViewBehaviours
+import views.html.transport.equipment.index.RemoveTransportEquipmentView
+
+class RemoveTransportEquipmentViewSpec extends YesNoViewBehaviours {
+
+  override def applyView(form: Form[Boolean]): HtmlFormat.Appendable =
+    injector.instanceOf[RemoveTransportEquipmentView].apply(form, lrn, NormalMode)(fakeRequest, messages)
+
+  override val prefix: String = "transport.equipment.index.removeTransportEquipment"
+
+  behave like pageWithTitle()
+
+  behave like pageWithBackLink()
+
+  behave like pageWithHeading()
+
+  behave like pageWithRadioItems()
+
+  behave like pageWithSubmitButton("Save and continue")
+}
