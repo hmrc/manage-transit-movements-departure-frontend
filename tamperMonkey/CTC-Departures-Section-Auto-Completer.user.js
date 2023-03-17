023c3ddc34e6b285e6dfc1d4a867bb2352024ec9
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2023-03-17 09:55:35 +0000

    bump up version number

--- tamperMonkey/CTC-Departures-Section-Auto-Completer.user.js
+++ tamperMonkey/CTC-Departures-Section-Auto-Completer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CTC-Departures Section Auto Completer
 // @namespace    http://tampermonkey.net/
-// @version      9.0
+// @version      10.0
 // @description  Script to automatically fill out CTC sections
 // @author       Reece-Carruthers
 // @match        http*://*/manage-transit-movements/departures/*/task-list
