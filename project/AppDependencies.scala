93688bc5a7043ba8ebe184476959be7206cf44d7
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-07-02 13:18:38 +0100

    CTCP-5506: update dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"   % "2.0.0",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "10.1.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "10.3.0",
     "org.typelevel"              %% "cats-core"                               % "2.10.0",
     "com.chuusai"                %% "shapeless"                               % "2.3.12",
     "org.apache.commons"          % "commons-text"                            % "1.11.0"
