f2e4b9850634ad5dd52166701c4dbb6d9f5794b4
Kevin Coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2024-10-03 15:21:40 +0100

    CTCP-2273: refactoring scaffold inputSelectPage

--- .g8/inputSelectPage/generated-test/views/$package$/$className$ViewSpec.scala
+++ .g8/inputSelectPage/generated-test/views/$package$/$className$ViewSpec.scala
@@ -30,7 +30,5 @@ class $className$ViewSpec extends InputSelectViewBehaviours[$referenceClass$] {
 
   behave like pageWithHint("$title$ hint")
 
-  behave like pageWithContent("label", "$title$ label")
-
   behave like pageWithSubmitButton("Save and continue")
 }
