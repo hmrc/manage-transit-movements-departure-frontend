8296046426edeecc5fb5832540bb6e7c8ce6ba20
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2024-10-16 15:19:13 +0100

    CTCP-5963: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.3.0"
+  private val bootstrapVersion = "9.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"   % "3.2.0",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "10.9.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "10.13.0",
     "org.typelevel"              %% "cats-core"                               % "2.12.0",
     "org.apache.commons"          % "commons-text"                            % "1.11.0"
   )
