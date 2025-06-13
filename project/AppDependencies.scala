50786dc8e6faee11ae11a686b6bd3cb992991b5d
Roshan Gautam <106965552+Roshangautam2052@users.noreply.github.com>
2025-06-13 12:03:06 +0100

    CTCP-6533 Refactor exceptions in recover expressions with NonFatal (#686)
    * CTCP-6533 Refactor exceptions in recover expressions with NonFatal
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
    
    * CTCP-6533 Update dependencies
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.12.0"
+  private val bootstrapVersion = "9.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"   % "3.3.0",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "12.1.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "12.4.0",
     "org.typelevel"              %% "cats-core"                               % "2.13.0",
     "org.apache.commons"          % "commons-text"                            % "1.13.1"
   )
