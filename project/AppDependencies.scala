638a1acb8fd2d8abb8cb57746c953ad30f81be28
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-07-07 13:46:06 +0100

    CTCP-6571: Dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,14 +1,13 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
+  private val bootstrapVersion = "9.14.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"   % "3.3.0",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "12.6.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "12.7.0",
     "org.typelevel"              %% "cats-core"                               % "2.13.0",
     "org.apache.commons"          % "commons-text"                            % "1.13.1"
   )
