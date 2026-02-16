e2110b631f8850f954ae58ced872a66a605a61dd
damien.butterworth <damienbutterworth@hotmail.co.uk>
2026-02-16 11:28:59 +0000

    Upgrade bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "12.11.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "12.31.0",
     "org.typelevel"              %% "cats-core"                               % "2.13.0",
     "org.apache.commons"          % "commons-text"                            % "1.14.0"
   )
